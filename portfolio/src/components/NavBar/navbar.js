8436457e1dd55bf4329dd4abec81acb205d4845a
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-23 03:07:28 +0530

    Committed

---
+++ portfolio/src/components/NavBar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+    return (
+    <div>
+        Navbar
+        <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+    </div>
+    );
+};
+
+export default Navbar;
